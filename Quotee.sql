7b3be3c25a95c8035a930363e7dd609b9115a913
lnatalo712 <58570087+lnatalo712@users.noreply.github.com>
2019-12-09 15:08:19 -0500

    Update Quotee.sql

--- Quotee.sql
+++ Quotee.sql
@@ -9,17 +9,17 @@ CREATE TABLE QUOTEE(
   CONSTRAINT Quotee_Id PRIMARY KEY (QuoteeId)
 );
 
-INSERT INTO Quotee(QuoteeId, FName, MName, LName, Quote)
-VALUES(1, 'Mother', NULL, 'Teresa', 'Spread love everywhere you go. Let no one ever come to you without leaving happier.');
+INSERT INTO Quotee VALUES
+(1, 'Mother', NULL, 'Teresa', 'Spread love everywhere you go. Let no one ever come to you without leaving happier.');
 
-INSERT INTO Quotee(QuoteeId, FName, MName, LName, Quote)
-VALUES(2, 'Franklin', 'D.', 'Roosevelt', 'When you reach the end of your rope, tie a knot in it and hang on.');
+INSERT INTO Quotee VALUES
+(2, 'Franklin', 'D.', 'Roosevelt', 'When you reach the end of your rope, tie a knot in it and hang on.');
 
-INSERT INTO Quotee(QuoteeId, FName, MName, LName, Quote)
-VALUES(3, 'Margaret', NULL, 'Mead', 'Always remember that you are absolutely unique. Just like everyone else.');
+INSERT INTO Quotee VALUES
+(3, 'Margaret', NULL, 'Mead', 'Always remember that you are absolutely unique. Just like everyone else.');
 
-INSERT INTO Quotee(QuoteeId, FName, MName, LName, Quote)
-VALUES(4, 'Robert', 'Louis', 'Stevenson', 'Don\'t judge each day by the harvest you reap but by the seeds that you plant.');
+INSERT INTO Quotee VALUES
+(4, 'Robert', 'Louis', 'Stevenson', 'Don\'t judge each day by the harvest you reap but by the seeds that you plant.');
 
-INSERT INTO Quotee(QuoteeId, FName, MName, LName, Quote)
-VALUES(5, 'Eleanor', NULL, 'Roosevelt', 'The future belongs to those who believe in the beauty of their dreams.');
+INSERT INTO Quotee VALUES
+(5, 'Eleanor', NULL, 'Roosevelt', 'The future belongs to those who believe in the beauty of their dreams.');
